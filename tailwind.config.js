7f271394c68b379eccd354271eee2aebdb292530
Emmanuel Oppong <107645887+emmaeng680@users.noreply.github.com>
2024-11-28 09:49:11 -0600

    update
    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,26 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	container: {
+		center: true,
+		padding: "15px",
+	},
+  screens: {
+		sm: "640px",
+		md: "768px",
+		lg: "960px",
+		xl: "1200px",
+  },
+  fontFamily:{
+		primary: "var(--font-jetbrainsMono)",
+  },
   	extend: {
-  		colors: {
+		colors: {
+			primary:"#1c1c22",
+			accent:{
+				DEFAULT: "#00ff99",
+				hover: "#00e187",
+			},
   			background: 'var(--background)',
   			foreground: 'var(--foreground)'
   		},
